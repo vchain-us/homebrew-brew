ca489421547c57fc0c47b8a8db65d13bc7e1d05d
Leonardo Grasso <me@leonardograsso.com>
2019-10-04 11:56:47 +0200

    chore: version v0.7.0

--- Formula/vcn.rb
+++ Formula/vcn.rb
@@ -1,20 +1,20 @@
 class Vcn < Formula
     desc "VChain Code Notary Command-Line Interface"
     homepage "https://www.codenotary.io"
-    version "v0.6.3"
+    version "v0.7.0"
     if OS.mac?
-        url "https://github.com/vchain-us/vcn/releases/download/v0.6.3/vcn-v0.6.3-darwin-10.6-amd64"
-        sha256 "5a9b44c4e570975fe65e3f82cc5dc4bfdf358f7b3cef9166e5a2544bba347053"
+        url "https://github.com/vchain-us/vcn/releases/download/v0.7.0/vcn-v0.7.0-darwin-10.6-amd64"
+        sha256 "0b8f652fd9ee2fbd8a793fc3dcd387b0e22b85652db021e6f6f90be73d3d8088"
     elsif OS.linux?
-        url "https://github.com/vchain-us/vcn/releases/download/v0.6.3/vcn-v0.6.3-linux-amd64"
-        sha256 "cabea5ccdf9380775f1d40fd2a1baec8ee697ecf107f13283bcfc08bd0c9df65"
+        url "https://github.com/vchain-us/vcn/releases/download/v0.7.0/vcn-v0.7.0-linux-amd64"
+        sha256 "943a62c7043db6479628a2742f728bff2537359787da88360ce1aa761b7819e9"
     end
 
     def install
         if OS.mac?
-            bin.install "./vcn-v0.6.3-darwin-10.6-amd64" => "vcn"
+            bin.install "./vcn-v0.7.0-darwin-10.6-amd64" => "vcn"
         elsif OS.linux?
-            bin.install "./vcn-v0.6.3-linux-amd64" => "vcn"
+            bin.install "./vcn-v0.7.0-linux-amd64" => "vcn"
         end
     end
 
