741333a25b5ac1a76abe748bef717990577c8008
Leonardo Grasso <me@leonardograsso.com>
2019-10-30 17:33:53 +0100

    chore: version v0.7.3

--- Formula/vcn.rb
+++ Formula/vcn.rb
@@ -1,13 +1,13 @@
 class Vcn < Formula
     desc "VChain Code Notary Command-Line Interface"
     homepage "https://www.codenotary.io"
-    version "v0.7.2"
+    version "v0.7.3"
     if OS.mac?
         url "https://github.com/vchain-us/vcn/releases/download/#{version}/vcn-#{version}-darwin-10.6-amd64"
-        sha256 "72eccc89b0ab1f7b51a69b233e83af02d8ffa5b163ca1eff313d33f9420e94c0"
+        sha256 "c05c8a289271f328a0fd375b058001d5157c637f5ee73a225ca3bab90383f21e"
     elsif OS.linux?
         url "https://github.com/vchain-us/vcn/releases/download/#{version}/vcn-#{version}-linux-amd64"
-        sha256 "5b50b3acec0f9f7cfe4f8a60401d4d1672a795c8bc95f3408192d18ccec8d160"
+        sha256 "e44d2a9264b542566ee93ac5400b48bc4e024beac10c762a6b2d2ea0da3baa5e"
     end
 
     def install
