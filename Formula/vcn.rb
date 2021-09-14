4d24226e5d328e21ff32ec14d812aec26c2f99b9
Dennis <60224695+vchaindz@users.noreply.github.com>
2021-09-14 21:06:39 +0200

    update vcn 0.9.9

--- Formula/vcn.rb
+++ Formula/vcn.rb
@@ -1,13 +1,13 @@
 class Vcn < Formula
-    desc "VChain Code Notary Command-Line Interface"
-    homepage "https://www.codenotary.io"
-    version "v0.8.3"
+    desc "Codenotary Cloud Command-Line Interface"
+    homepage "https://www.codenotary.com"
+    version "v0.9.9"
     if OS.mac?
         url "https://github.com/vchain-us/vcn/releases/download/#{version}/vcn-#{version}-darwin-amd64"
-        sha256 "8f6dd3b51404537f5fbfff46b9967deaa2fa66fef270082337eceaabeb9f1e2b"
+        sha256 "d39824501bfe6e3ffcc91c4cf458650ba4f3a86a3f7f57e2209d5de7aac246de"
     elsif OS.linux?
         url "https://github.com/vchain-us/vcn/releases/download/#{version}/vcn-#{version}-linux-amd64"
-        sha256 "78346fce90072ab39287645ff3a3acbca4a88386f646b4a2eac40c1481215dfe"
+        sha256 "0afff16bd636c1a53437c389736848186670f31096785779392fa66c3b03a803"
     end
 
     def install
